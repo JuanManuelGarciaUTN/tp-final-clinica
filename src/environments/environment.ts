19a606250eb104e87ad4619be64db4f87bb3a640
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2023-06-01 20:55:39 -0300

    favico y hosting finish

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'clinica-tp-final',
+    appId: '1:861938174086:web:33c9f930f1aab7231be907',
+    storageBucket: 'clinica-tp-final.appspot.com',
+    apiKey: 'AIzaSyAC066ov3lle1ot_uaDX5AhSuFJ2NxOMuE',
+    authDomain: 'clinica-tp-final.firebaseapp.com',
+    messagingSenderId: '861938174086',
+  },};
